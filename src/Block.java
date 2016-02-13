601d4ac4f7c1790d674f82e9d5db46c575ff4d0b
Nick <nick@Danas-MacBook-Pro.local>
2016-02-13 14:34:23 -0500

    Statics

--- src/Block.java
+++ src/Block.java
@@ -2,6 +2,10 @@ import java.awt.Graphics;
 
 public class Block extends Shape{
 
+	public Block(double x, double y) {
+		super(x, y);
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
--- src/Shape.java
+++ src/Shape.java
@@ -3,4 +3,8 @@ public class Shape {
 	Vector velocity;
 	Vector position;
 	Vector acceleration;
+	
+	public Shape(double x, double y){
+		velocity = new Vector(x, y);
+	}
 }
