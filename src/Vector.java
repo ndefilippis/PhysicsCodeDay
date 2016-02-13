c6185884ce8bdcf095a17f2231884d569c94be10
Nick <nick@Danas-MacBook-Pro.local>
2016-02-13 13:33:13 -0500

    Vector Math

--- src/Vector.java
+++ src/Vector.java
@@ -1,4 +1,38 @@
 
 public class Vector {
-
+	public double x, y;
+	
+	
+	
+	public Vector(double x, double y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public Vector add(Vector v){
+		return new Vector(x+v.x, y+v.y);
+	}
+	public Vector subtract(Vector v){
+		return new Vector(x-v.x, y-v.y);
+	}
+	
+	public Vector multiply(double s){
+		return new Vector(x*s, y*s);
+	}
+	
+	public Vector divide(double s){
+		return multiply(1.0/s);
+	}
+	
+	public double dot(Vector v){
+		return x*v.x + y*v.y;
+	}
+	
+	public double lengthSquare(){
+		return 
+	}s
+	
+	public Vector normalize(){
+		
+	}
 }
