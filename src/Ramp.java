3a6fa6992c79baf968543e733cba0b7b6f35faa8
DanielWilliamSwartz <DanSwartz@Dannnys-MacBook-Pro.local>
2016-02-13 15:09:35 -0500

    added ramp

--- src/Ramp.java
+++ src/Ramp.java
@@ -1,13 +1,23 @@
 import java.awt.Graphics;
 
-public class Ramp{
+public class Ramp extends Shape{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public Ramp(double x, double y) {
+		super(x, y);
+		// TODO Auto-generated constructor stub
 	}
+	
 	public void draw(Graphics g){
-		
+		int [] x = {0,300,300};
+		int [] y = {1600, 1200};
+		g.drawPolygon(x,y,3);
 	}
+	
+	@Override
+	public double friction(Shape s) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
 
 }
