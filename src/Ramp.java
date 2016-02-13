2f51bcd1fd09fc43a35b79bc3a1a6f6a34e547b3
DanielWilliamSwartz <DanSwartz@Dannnys-MacBook-Pro.local>
2016-02-13 15:17:01 -0500

    sdafsa

--- src/Ramp.java
+++ src/Ramp.java
@@ -2,14 +2,14 @@ import java.awt.Graphics;
 
 public class Ramp extends Shape{
 
-	public Ramp(double x, double y) {
-		super(x, y);
+	public Ramp(double x, double y, double width, double height) {
+		super(x, y, width, height);
 		// TODO Auto-generated constructor stub
 	}
 	
 	public void draw(Graphics g){
-		int [] x = {0,300,300};
-		int [] y = {1600, 1200};
+		int [] x = {(int) position.x,(int) width,(int) width};
+		int [] y = {(int) position.y, (int) height, (int) position.y};
 		g.drawPolygon(x,y,3);
 	}
 	
